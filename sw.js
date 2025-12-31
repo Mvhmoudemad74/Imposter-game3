e467b85e4872c687257dad6278f2c64e28039c5b
Mahmoud.github.io <me8587859@gmail.com>
2025-12-31 06:50:37 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,10 @@
-const CACHE_NAME = "pwa-cache";
-const urlsToCache = [
-  "/Imposter-game3/",
-  "/Imposter-game3/index.html",
-  "/Imposter-game3/icon-192.png",
-  "/Imposter-game3/icon-512.png"
-];
+const cacheName = 'imposter-v1';
+const assets = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
-self.addEventListener("install", function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
 
-self.addEventListener("fetch", function(event) {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
